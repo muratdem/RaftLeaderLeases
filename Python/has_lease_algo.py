97d84a5ff3a2a21fd19014e864f5c244458638bc
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2024-06-11 15:19:16 -0400

    update python logic for paper

--- Python/has_lease_algo.py
+++ Python/has_lease_algo.py
@@ -3,34 +3,31 @@
 See lease_guard.py for the executable version.
 """
 
-def has_lease(self, for_writes: bool) -> bool:
-  if self.role is not LEADER:
-    return False
-  # Need lease to advance commit index, need to
-  # advance it to get a lease! So calc it from
-  # match_index. If I heard of a higher commit index
-  # while I was a follower, use that.
-  commit_idx = max(
-    self.commit_idx, median(self.match_index))
-  entries = self.log[:commit_idx + 1]
+def has_lease(self, for_commit: bool) -> bool:
+  if self.role is not PRIMARY: return False
   # Compensate for max clock drift.
   lease_start = (self.clock.now()
-   - lease_duration * (1 + max_clock_error))
-  # Last committed entry's before lease timeout?
-  if entries[-1].local_ts <= lease_start:
-    return False
-  # I can serve reads with inherited lease from old
-  # leader, but must commit my own entry for writes.
-  if for_writes:
-    if entries[-1].term != self.current_term:
-      # I haven't committed an entry yet.
-      return False
-    # Last entry I got from the previous leader.
-    prior_entry = next(
-      (e for e in reversed(entries)
-       if e.term != self.current_term))
-    # local_ts is my clock when I replicated entry.
-    if prior_entry.local_ts >= lease_start:
-      # Previous leader still has write lease.
+    - lease_timeout * (1 + max_clock_error))
+  if for_commit:
+    # If I replicated a past term's entry
+    # < lease_timeout ago, past leader may
+    # have a lease.
+    for e in reversed(self.log):
+      if e.term == self.current_term:
+        continue
+      if e.time_when_i_replicated >= lease_start:
+        # Past leader may have lease.
+        return False
+      # Don't need to check older entries.
+      break
+  else:
+    # Check if I have a lease for reads.
+    if self.commit_index == -1:
+      return False  # Can't read without commits.
+    # "Inherited read lease" means I can read
+    # while a prior leader's lease is valid.
+    entry = self.log[self.commit_index]
+    if entry.time_when_i_replicated < lease_start:
+      # Last committed entry is too old.
       return False
   return True
